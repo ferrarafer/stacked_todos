fce83c17cd30f9bf561947c0ed7499752407d7c6
Fernando Ferrara <ferrarafer@gmail.com>
2022-11-21 17:41:34 -0300

    Create project with stacked_cli

---
+++ lib/ui/setup/setup_dialog_ui.dart
@@ -0,0 +1,15 @@
+import 'package:todos/app/app.locator.dart';
+import 'package:todos/enums/dialog_type.dart';
+import 'package:todos/ui/dialogs/info_alert/info_alert_dialog.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+void setupDialogUi() {
+  final dialogService = locator<DialogService>();
+
+  final Map<dynamic, DialogBuilder> builders = {
+    DialogType.infoAlert: (context, sheetRequest, completer) =>
+        InfoAlertDialog(request: sheetRequest, completer: completer),
+  };
+
+  dialogService.registerCustomDialogBuilders(builders);
+}
