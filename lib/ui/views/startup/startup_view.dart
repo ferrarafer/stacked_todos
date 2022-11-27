7061ec3ff1ee8c31725c8325c0d3e419f4c8b250
Fernando Ferrara <ferrarafer@gmail.com>
2022-11-26 23:26:09 -0300

    Update StartpView
    - Add filledstacks logo to assets
    - Update StartupView
    - Install package logger
    - Install package uuid
    - Update package stacked

--- lib/ui/views/startup/startup_view.dart
+++ lib/ui/views/startup/startup_view.dart
@@ -5,51 +5,62 @@ import 'package:todos/ui/common/ui_helpers.dart';
 
 import 'startup_viewmodel.dart';
 
-class StartupView extends StatelessWidget {
+class StartupView extends ViewModelBuilderWidget<StartupViewModel> {
   const StartupView({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return ViewModelBuilder<StartupViewModel>.reactive(
-      builder: (context, model, child) => Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const Text(
-                'TodoMV* with STACKED',
-                style: TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.w900,
+  Widget builder(
+      BuildContext context, StartupViewModel viewModel, Widget? child) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text(
+              'TodoMVVM with STACKED',
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            ),
+            verticalSpaceMedium,
+            const SizedBox(
+              width: 50,
+              height: 50,
+              child: CircularProgressIndicator(
+                color: Colors.white,
+                strokeWidth: 6,
+              ),
+            ),
+            verticalSpaceMassive,
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    const Text(
+                      'Powered by',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    verticalSpaceTiny,
+                    Image.asset('assets/images/filledstacks_logo.png'),
+                  ],
                 ),
-              ),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: const [
-                  Text(
-                    'Loading ...',
-                    style: TextStyle(
-                      fontSize: 16,
-                    ),
-                  ),
-                  horizontalSpaceSmall,
-                  SizedBox(
-                    width: 16,
-                    height: 16,
-                    child: CircularProgressIndicator(
-                      color: Colors.black,
-                      strokeWidth: 6,
-                    ),
-                  )
-                ],
-              ),
-            ],
-          ),
+              ],
+            ),
+          ],
         ),
       ),
-      onModelReady: (model) => SchedulerBinding.instance
-          .addPostFrameCallback((timeStamp) => model.runStartupLogic()),
-      viewModelBuilder: () => StartupViewModel(),
     );
+  }
+
+  @override
+  void onViewModelReady(StartupViewModel viewModel) {
+    SchedulerBinding.instance.addPostFrameCallback(
+      (timeStamp) => viewModel.runStartupLogic(),
+    );
+  }
+
+  @override
+  StartupViewModel viewModelBuilder(BuildContext context) {
+    return StartupViewModel();
   }
 }
--- lib/ui/views/startup/startup_viewmodel.dart
+++ lib/ui/views/startup/startup_viewmodel.dart
@@ -8,10 +8,7 @@ class StartupViewModel extends BaseViewModel {
 
   // Place anything here that needs to happen before we get into the application
   Future runStartupLogic() async {
-    await Future.delayed(const Duration(seconds: 3));
-
-    // This is where you can make decisions on where your app should navigate when
-    // you have custom startup logic
+    await Future.delayed(const Duration(seconds: 2));
 
     _navigationService.replaceWith(Routes.homeView);
   }
